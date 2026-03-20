9408682f3be1e9493975cbae31ab00b0ba269cfe
Nikesh-koila <kirthanbirwaz43@gmail.com>
2026-03-20 21:55:23 +0530

    Practiced Binary Search on Answers Problems

---
+++ src/BinarySearch/BSonAnswers/MedianOfSortedArray.java
@@ -0,0 +1,92 @@
+package BinarySearch.BSonAnswers;
+
+public class MedianOfSortedArray {
+
+    //optimal approach
+    public static double findMedianSortedArrays1(int[] nums1, int[] nums2) {
+        int n1=nums1.length;
+        int n2=nums2.length;
+        if(n1>n2)return findMedianSortedArrays(nums2,nums1);
+        int left=(n1+n2+1)/2;
+
+        int low=0;
+        int high=n1;
+
+        while(low<=high){
+            int mid1=low+(high-low)/2;
+            int mid2=left-mid1;
+            int l1=Integer.MIN_VALUE;
+            int l2=Integer.MIN_VALUE;
+            int r1=Integer.MAX_VALUE;
+            int r2=Integer.MAX_VALUE;
+
+            if(mid1<n1)r1=nums1[mid1];
+            if(mid2<n2)r2=nums2[mid2];
+            if(mid1-1>=0)l1=nums1[mid1-1];
+            if(mid2-1>=0)l2=nums2[mid2-1];
+
+            if(l1<=r2 && l2<=r1){
+                if((n1+n2)%2==1)return Math.max(l1,l2);
+                return ((double)Math.max(l1,l2)+Math.min(r1,r2))/2;
+            }
+
+            else if(l1>r2)high=mid1-1;
+            else low=mid1+1;
+
+        }
+        return -1;
+    }
+
+    //better approach
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n=nums1.length;
+        int m=nums2.length;
+        int ind2=(n+m)/2;
+        int ind1=ind2-1;
+        int ele1=0;
+        int ele2=0;
+        int p=0;
+        int q=0;
+        int cnt=0;
+
+        while(p<n && q<m){
+            if(nums1[p]<nums2[q]){
+                if(cnt==ind1)ele1=nums1[p];
+                if(cnt==ind2)ele2=nums1[p];
+                cnt++;
+                p++;
+            }
+            else{
+                if(cnt==ind1)ele1=nums2[q];
+                if(cnt==ind2)ele2=nums2[q];
+                cnt++;
+                q++;
+            }
+        }
+
+        while(p<n){
+            if(cnt==ind1)ele1=nums1[p];
+            if(cnt==ind2)ele2=nums1[p];
+            cnt++;
+            p++;
+        }
+
+        while(q<m){
+            if(cnt==ind1)ele1=nums2[q];
+            if(cnt==ind2)ele2=nums2[q];
+            cnt++;
+            q++;
+        }
+        if((n+m)%2==1)
+            return ele2;
+        return ((double)ele1+ele2)/2;
+
+
+    }
+
+    public static void main(String[]args){
+        int[]a={1,2};
+        int[]b={3,4};
+        System.out.println(findMedianSortedArrays1(a,b));
+    }
+}
